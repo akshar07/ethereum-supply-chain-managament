4f92d6cfde835e37a140ca552e94e476f124e86c
akshar <takleakshar@gmail.com>
2018-02-15 16:50:14 -0800

    awesome

--- server-ui/server/routes/routes.js
+++
@@ -1,7 +0,0 @@
-//server/routes/routes.js
-var express = require('express');
-var router = express.Router();
-router.get('/', function(req, res){
-  res.render('index')
-});
-module.exports = router;
